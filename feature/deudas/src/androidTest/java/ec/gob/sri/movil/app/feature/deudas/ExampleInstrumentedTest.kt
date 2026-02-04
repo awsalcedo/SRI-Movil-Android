d1baf3a96cd2c6baa7a7c5f86a85c27e033bab4c
Alex Salcedo <awsalcedo@sri.gob.ec>
2026-02-04 16:54:48 -0500

    feat: Añadir el nuevo módulo `feature:deudas`
    Se ha creado un nuevo módulo de funcionalidad (`feature:deudas`) para albergar la lógica y la interfaz de usuario relacionadas con la consulta de deudas.
    
    - Se añade `include(":feature:deudas")` en `settings.gradle.kts` para incorporar el nuevo módulo al proyecto.
    - Se ha generado la estructura básica del módulo Android, incluyendo:
        - `build.gradle.kts` con las configuraciones y dependencias iniciales.
        - `AndroidManifest.xml`.
        - Ficheros `.gitignore`, `proguard-rules.pro` y `consumer-rules.pro`.
        - Un placeholder para el servicio de red (`DeudasService`).
        - Archivos de prueba unitaria e instrumentada de ejemplo.

---
+++ feature/deudas/src/androidTest/java/ec/gob/sri/movil/app/feature/deudas/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package ec.gob.sri.movil.app.feature.deudas
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("ec.gob.sri.movil.app.feature.deudas.test", appContext.packageName)
+    }
+}
---
+++ feature/deudas/src/main/java/ec/gob/sri/movil/app/feature/deudas/data/remote/service/DeudasService.kt
@@ -0,0 +1,5 @@
+package ec.gob.sri.movil.app.feature.deudas.data.remote.service
+
+interface DeudasService {
+    suspend fun consultarApi()
+}
